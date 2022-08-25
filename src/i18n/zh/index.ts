e348a21abed749bda48453d4cd0b857b851c3008
mkpoli <snowwolf1001@gmail.com>
2022-08-26 01:11:55 +0900

    Add footer and title

--- src/i18n/zh/index.ts
+++ src/i18n/zh/index.ts
@@ -11,6 +11,9 @@ const zh: Translation = {
     'placeholder': '請在此輸入新的句子...',
     'add': '添加',
     'guidance': '空格和標點符號自動分詞。使用{delimiter}來進一步分隔或用於中日韓泰藏等不使用空格分詞的文本，例如{example}',
+  },
+  footer: {
+    'info': '使用本應用生成的圖像於{license}釋出，意即公開於公用領域的，完全自由使用。不過也歡迎分享並在使用時標註出處。',
   }
 }
 
