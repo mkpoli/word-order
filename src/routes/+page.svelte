36b6d09043f70b267d92ef3203d7f90895e8221e
mkpoli <snowwolf1001@gmail.com>
2022-08-26 03:58:24 +0900

    Componentise Parameters

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,16 @@
 <script lang="ts">
-	import { pickNColors } from '$lib/color';
+	import { pickNColors } from '../lib/color';
 	import * as d3 from 'd3-color';
 	import { onMount, tick } from 'svelte';
-	import SentenceInput from '$lib/SentenceInput.svelte';
+	import SentenceInput from '../lib/SentenceInput.svelte';
 	import 'iconify-icon';
 
 	import { LL } from '../i18n/i18n-svelte';
 
-	import Output, { type Line } from '$lib/Output.svelte';
+	import Output, { type Line } from '../lib/Output.svelte';
 	import type { Alignment, Mode } from '$lib/types';
 
+	import Parameters from '$lib/Parameters.svelte';
 
 	// const SENTENCES = [
 	// 	['en', "I can eat glass and it doesn't hurt me."],
@@ -43,8 +44,8 @@
 	let word_spans: HTMLSpanElement[][];
 
 	// Parameters
-	let verticalGap = 30;
-	let lineGap = 5;
+	let verticalGap: number;
+	let lineGap: number;
 	let alignment: Alignment = 'center';
 
 	let mounted = false;
@@ -116,29 +117,7 @@
 
 <main>
 	<div class="params">
-		<label for="vertical-gap">
-			<iconify-icon icon="mdi:arrow-expand-vertical" inline="true" />
-			{$LL.params.verticalGap()} ({verticalGap}px)
-		</label>
-		<input type="range" bind:value={verticalGap} id="vertical-gap" name="vertical-gap" min="0" max="100" />
-		<label for="line-gap">
-			<iconify-icon icon="mdi:arrow-split-horizontal" inline="true" />
-			{$LL.params.lineGap()} ({lineGap}px)
-		</label>
-		<input type="range" bind:value={lineGap} id="line-gap" name="line-gap" min="-5" max={verticalGap / 2} />
-
-		<label for="alignment">{$LL.params.textAlignment()}</label>
-		<div class="alignment">
-			<input type="radio" bind:group={alignment} name="alignment" value="left" id="alignment-left" />
-			<label for="alignment-left"><iconify-icon icon="ic:round-format-align-left" /></label>
-			<input type="radio" bind:group={alignment} name="alignment" value="center" id="alignment-center" />
-			<label for="alignment-center"><iconify-icon icon="ic:round-format-align-center" /></label>
-			<input type="radio" bind:group={alignment} name="alignment" value="right" id="alignment-right" />
-			<label for="alignment-right"><iconify-icon icon="ic:round-format-align-right" /></label>
-		</div>
-
-		<label for="locale">{$LL.params.displayLanguage()}</label>
-		<LocaleSelect />
+		<Parameters bind:verticalGap bind:lineGap bind:alignment />
 	</div>
 
 	<div class="input">
