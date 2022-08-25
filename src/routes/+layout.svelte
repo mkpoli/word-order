75de7702067092618f93ee0013461a5ab2c043ab
mkpoli <snowwolf1001@gmail.com>
2022-08-26 03:43:45 +0900

    Fix locale not loading

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
 	import { setLocale } from '$i18n/i18n-svelte';
+	import type { Locales } from '$i18n/i18n-types';
+	import { loadLocale } from '$i18n/i18n-util.sync';
 
 	export let data;
 
@@ -7,7 +9,11 @@
 
 	if (typeof window !== 'undefined') {
 		const lang = window.localStorage.getItem('locale');
+
 		if (lang) {
+			if (lang !== locale) {
+				loadLocale(lang as Locales);
+			}
 			locale = lang;
 			window.localStorage.setItem('locale', lang);
 		}
--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -3,11 +3,12 @@ import { loadLocaleAsync } from '$i18n/i18n-util.async';
 import type { LayoutLoad } from './$types';
 
 export const load: LayoutLoad<{ locale: Locales }> = async ({ data }) => {
-  const locale = (data as unknown as {
-    locale: Locales
-  }).locale;
+  let locale = null
+  if (data && (data as { locale: Locales }).locale) {
+    locale = (data as { locale: Locales }).locale
+    await loadLocaleAsync(locale);
+  }
 
-  await loadLocaleAsync(locale);
   return {
     locale
   };
