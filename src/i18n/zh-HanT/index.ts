0e9953b7aa933782e3c43b42d2d94b3ddfde8507
mkpoli <snowwolf1001@gmail.com>
2022-08-27 15:32:44 +0900

    Adjust appearance of input-form

--- src/i18n/zh-HanT/index.ts
+++ src/i18n/zh-HanT/index.ts
@@ -22,6 +22,7 @@ const zh: Translation = {
     'fontSize': '字體大小',
   },
   input: {
+    'input': '輸入',
     'placeholder': '請在此輸入新的句子...',
     'add': '添加',
     'guidance': '空格和標點符號自動分詞。使用{delimiter}來進一步分隔或用於中日韓泰藏等不使用空格分詞的文本，例如{example}',
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -24,3 +24,40 @@
 </script>
 
 <slot />
+
+<style global>
+	* {
+		box-sizing: border-box;
+	}
+
+	fieldset {
+		display: flex;
+		flex-direction: column;
+		gap: 0.4em;
+
+		padding: 1em 1.5em;
+		border-radius: 0.5em;
+		border: none;
+		box-shadow: 1px 1px 5px 0 #ccc;
+	}
+
+	legend {
+		background: white;
+		padding: 0.2em 0.8em;
+		border-radius: 0.3em;
+		box-shadow: 1px 1px 5px 0 #ccc;
+		font-weight: bold;
+	}
+
+	input[type='text'],
+	textarea {
+		padding: 0.3em 0.5em;
+		border-radius: 0.3em;
+		border: 1px solid #767676;
+	}
+
+	input[type='text']:hover,
+	textarea:hover {
+		border: 1px solid #4f4f4f;
+	}
+</style>
