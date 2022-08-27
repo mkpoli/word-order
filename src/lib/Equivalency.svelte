b40bcfa01c30edd069258228525ed24fd7fb4115
mkpoli <snowwolf1001@gmail.com>
2022-08-28 01:56:54 +0900

    Add color-bar

--- src/lib/Equivalency.svelte
+++ src/lib/Equivalency.svelte
@@ -1,26 +1,57 @@
 <script lang="ts">
+	import { pickNColors, lch2rgb } from '$lib/color';
+
 	export let sentences: [string, string[]][];
 	export let equivalency: number[][][];
 	export let colors: string[];
+
+	console.log(
+		`linear-gradient(to bottom, ${pickNColors(20)
+			.map(lch2rgb)
+			.map((c, i) => `${c} ${((i * 1) / 20) * 100}%`)
+			.join(', ')})`
+	);
 </script>
 
-{#each equivalency as entry, i}
-	<div class="equivalency" style={`color: ${colors[i]}`}>
-		{#each entry as words, j}
-			<span class="words">
-				{#if words.length === 0}
-					<span class="word">❌</span>
-				{:else}
-					{#each words as k}
-						<span class="word">{sentences[j][1][k]}</span>
-					{/each}
-				{/if}
-			</span>
-		{/each}
-	</div>
-{/each}
+<div
+	class="color-bar"
+	style:background-image={`linear-gradient(to bottom, ${pickNColors(15)
+		.map(lch2rgb)
+		.map((c, i) => `${c} ${((i * 1) / 15) * 100}%`)
+		.join(', ')})`}
+/>
+<div class="entries">
+	{#each equivalency as entry, i}
+		<div class="equivalency" style={`color: ${colors[i]}`}>
+			{#each entry as words, j}
+				<span class="words">
+					{#if words.length === 0}
+						<span class="word">❌</span>
+					{:else}
+						{#each words as k}
+							<span class="word">{sentences[j][1][k]}</span>
+						{/each}
+					{/if}
+				</span>
+			{/each}
+		</div>
+	{/each}
+</div>
 
 <style>
+	.entries {
+		display: flex;
+		flex-direction: column;
+		position: relative;
+		height: fit-content;
+	}
+
+	.color-bar {
+		/* position: absolute; */
+		width: 0.3em;
+		border-radius: 9999px;
+	}
+
 	.words:not(:last-of-type)::after {
 		content: '＝';
 	}
--- src/lib/color.ts
+++ src/lib/color.ts
@@ -1,3 +1,5 @@
+import * as d3 from 'd3-color';
+
 function* generateEvenlySpacedNumbers(start: number, end: number, n: number): Generator<number> {
   const distance = end - start;
   const step = distance / (n - 1);
@@ -7,8 +9,13 @@ function* generateEvenlySpacedNumbers(start: number, end: number, n: number): Ge
   }
 }
 
-type LCh = [number, number, number];
+type LCh = [l: number, c: number, h: number];
 
 export function pickNColors(n: number): LCh[] {
   return [...generateEvenlySpacedNumbers(0, 360, n + 1)].map(degrees => [40, 100, degrees]).slice(0, -1) as LCh[];
+}
+
+export function lch2rgb(lch: LCh): string {
+  const [l, c, h] = lch
+  return d3.lch(l, c, h).formatRgb();
 }
