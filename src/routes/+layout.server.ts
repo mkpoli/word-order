b19f3fda35e8d1d1e15c9c284d7de01eb46fa83f
mkpoli <git@mkpo.li>
2023-06-22 20:52:44 +0900

    Update dependencies

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,20 +1,13 @@
-import type { Locales } from '$i18n/i18n-types'
-import { initAcceptLanguageHeaderDetector } from 'typesafe-i18n/detectors'
-import type { LayoutServerLoad } from './$types'
-import { detectLocaleAliased } from '$i18n/alias'
+import type { Locales } from '$i18n/i18n-types';
+import { initAcceptLanguageHeaderDetector } from 'typesafe-i18n/detectors';
+import type { LayoutServerLoad } from './$types';
+import { detectLocaleAliased } from '$i18n/alias';
 
-function getHeaders(request: Request) {
-  const headers: Record<string, string> = {}
-  request.headers.forEach((value, key) => { headers[key] = value })
-  return headers
-}
+export const load: LayoutServerLoad<{ locale: Locales }> = ({ request: { headers } }) => {
+	const acceptLanguageDetector = initAcceptLanguageHeaderDetector({ headers });
+	const locale = detectLocaleAliased(acceptLanguageDetector);
 
-export const load: LayoutServerLoad<{ locale: Locales }> = ({ request }) => {
-  const headers = getHeaders(request)
-  const acceptLanguageDetector = initAcceptLanguageHeaderDetector({ headers })
-  const locale = detectLocaleAliased(acceptLanguageDetector)
-
-  return {
-    locale
-  };
-}
+	return {
+		locale
+	};
+};
