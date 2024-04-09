03e4cf3793eef6b79143a309206130ad06fab1ad
graphemecluster <graphemecluster@gmail.com>
2024-04-09 10:21:47 +0800

    ﻿Format Code

--- src/lib/color.ts
+++ src/lib/color.ts
@@ -1,21 +1,21 @@
 import Color from 'colorjs.io';
 
 function* generateEvenlySpacedNumbers(start: number, end: number, n: number): Generator<number> {
-  const distance = end - start;
-  const coprimeN = (n >>> 1) - +((n & 1) === 0) - +((n & 3) === 2);
-  const step = (distance / n) * coprimeN;
+	const distance = end - start;
+	const coprimeN = (n >>> 1) - +((n & 1) === 0) - +((n & 3) === 2);
+	const step = (distance / n) * coprimeN;
 
-  for (let i = 0; i < n; i++) {
-    yield start + i * step;
-  }
+	for (let i = 0; i < n; i++) {
+		yield start + i * step;
+	}
 }
 
 type LCh = [l: number, c: number, h: number];
 
 export function pickNColors(n: number): LCh[] {
-  return [...generateEvenlySpacedNumbers(0, 360, n)].map(degrees => [0.6, 0.25, degrees]) as LCh[];
+	return [...generateEvenlySpacedNumbers(0, 360, n)].map((degrees) => [0.6, 0.25, degrees]) as LCh[];
 }
 
 export function lch2rgb(lch: LCh): string {
-  return new Color('oklch', lch).to('srgb') + '';
+	return new Color('oklch', lch).to('srgb') + '';
 }
