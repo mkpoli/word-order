d52897203408cdad28e8ff1b8462bb2d25d8f133
graphemecluster <graphemecluster@gmail.com>
2024-04-09 10:21:47 +0800

    ﻿Format Code

--- src/i18n/alias.ts
+++ src/i18n/alias.ts
@@ -1,25 +1,24 @@
-import { baseLocale, locales } from './i18n-util'
-import type { Locales } from "./i18n-types";
+import { baseLocale, locales } from './i18n-util';
+import type { Locales } from './i18n-types';
 import { detectLocale, type LocaleDetector } from 'typesafe-i18n/detectors';
 
 export const LOCALE_ALIAS = new Map<string, Locales>([
-  ['zh-CN', 'zh-HanS'],
-  ['zh-TW', 'zh-HanT'],
-  ['zh-HK', 'zh-HanT'],
-  ['zh', 'zh-HanS'],
-])
+	['zh-CN', 'zh-HanS'],
+	['zh-TW', 'zh-HanT'],
+	['zh-HK', 'zh-HanT'],
+	['zh', 'zh-HanS']
+]);
 
 export function getAliasedLocale(locale: string): Locales {
-  const aliasedLocale = (LOCALE_ALIAS.get(locale) ?? locale) as Locales
-  return locales.includes(aliasedLocale) ? aliasedLocale : baseLocale
+	const aliasedLocale = (LOCALE_ALIAS.get(locale) ?? locale) as Locales;
+	return locales.includes(aliasedLocale) ? aliasedLocale : baseLocale;
 }
 
 export function getAllLocales(): string[] {
-  return [...locales, ...LOCALE_ALIAS.keys()];
+	return [...locales, ...LOCALE_ALIAS.keys()];
 }
 
 export function detectLocaleAliased(detector: LocaleDetector): Locales {
-  const locale = detectLocale<Locales>(baseLocale, getAllLocales() as Locales[], detector)
-  return getAliasedLocale(locale)
+	const locale = detectLocale<Locales>(baseLocale, getAllLocales() as Locales[], detector);
+	return getAliasedLocale(locale);
 }
-
--- src/lib/array.ts
+++ src/lib/array.ts
@@ -1,31 +1,31 @@
 export function segmentate(arr: number[]): number[][] {
-  const array = [...new Set(arr)].sort((a, b) => a - b);
-  const result: number[][] = [];
+	const array = [...new Set(arr)].sort((a, b) => a - b);
+	const result: number[][] = [];
 
-  let segment: [start: number, end: number] = [array[0], array[0]];
-  for (let i = 0; i <= array.length - 1; i++) {
-    const [curr, next] = [array[i], array[i + 1]];
+	let segment: [start: number, end: number] = [array[0], array[0]];
+	for (let i = 0; i <= array.length - 1; i++) {
+		const [curr, next] = [array[i], array[i + 1]];
 
-    if (curr + 1 === next) {
-      segment = [segment[0], next];
-    } else {
-      result.push(segment);
-      segment = [next, next];
-    }
-  }
-  return result;
+		if (curr + 1 === next) {
+			segment = [segment[0], next];
+		} else {
+			result.push(segment);
+			segment = [next, next];
+		}
+	}
+	return result;
 }
 
 export function cartesian<T>(...arrays: T[][]): T[][] {
-  const result: T[][] = [];
-  const length = arrays.length;
-  const product = arrays.reduce((a, b) => a * b.length, 1);
-  for (let i = 0; i < product; i++) {
-    const row = [];
-    for (let j = 0; j < length; j++) {
-      row.push(arrays[j][i % arrays[j].length]);
-    }
-    result.push(row);
-  }
-  return result;
+	const result: T[][] = [];
+	const length = arrays.length;
+	const product = arrays.reduce((a, b) => a * b.length, 1);
+	for (let i = 0; i < product; i++) {
+		const row = [];
+		for (let j = 0; j < length; j++) {
+			row.push(arrays[j][i % arrays[j].length]);
+		}
+		result.push(row);
+	}
+	return result;
 }
