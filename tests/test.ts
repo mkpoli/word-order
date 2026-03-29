a332089fd6bc589e8003bcc56ad7069d71ed7a25
mkpoli <git@mkpo.li>
2026-03-29 19:01:59 +0900

    test: update footer text assertion to use toContain instead of toBe

--- tests/test.ts
+++ tests/test.ts
@@ -2,5 +2,5 @@ import { expect, test } from '@playwright/test';
 
 test('index page has expected footer', async ({ page }) => {
 	await page.goto('/');
-	expect(await page.textContent('footer')).toBe('Word Order Illustrator');
+	expect(await page.textContent('footer')).toContain('Word Order Illustrator');
 });
