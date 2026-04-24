d144666618604559b6eeeb5e9648dc2d90506fc2
mkpoli <git@mkpo.li>
2026-04-25 02:49:59 +0900

    feat(i18n): add Ainu translation

---
+++ src/i18n/ain/index.ts
@@ -0,0 +1,70 @@
+import type { Translation } from '../i18n-types';
+
+const ain: Translation = {
+	meta: {
+		title: 'Itakpo Ukotukka A=nokakarpe'
+	},
+	params: {
+		options: 'Ieramasu',
+		verticalGap: 'Ikuspe uturu',
+		lineGap: 'Itakrunoka uturu',
+		straightLength: 'Owpeka serekehe',
+		endpointCorrection: 'Iotta ios owpekaka',
+		textAlignment: 'Itaknoka Uoma',
+		displayLanguage: 'Itak',
+		text: 'Itaksay',
+		fontFamily: 'Inuye noka',
+		default: 'Siurenkare',
+		serif: 'Itomka an',
+		sansSerif: 'Itomka isam',
+		monospace: 'Uneno sep',
+		fontStyle: 'Noka puri',
+		normal: 'Yayanpe',
+		italic: 'Ohewkep',
+		bold: 'Ruwep',
+		boldItalic: 'Ruweohewkep',
+		fontSize: 'Nep pakno poro p'
+	},
+	input: {
+		input: 'A=nuye usi',
+		placeholder: 'Teta asir itaksay a=nuye ro…',
+		add: 'O',
+		modify: 'Nuye',
+		guidance:
+			'itak eramuan kikay an yakun ani wa isam yakun ohap aytaksay’usarayep itakpo kosiusaraye. {delimiter} a=eywanke wa na nokanno a=usaraye easkay. {example} neno ene an hi ne.',
+	},
+	tokenEditor: {
+		tokens: 'Itakpo',
+		mergeSelected: 'A=numke p kote',
+		splitAtBoundary: 'Teta usaraye',
+		selectTokens: 'Itakpo numke'
+	},
+	footer: {
+		info: 'Tan aeywankep or wa e=kar noka anakne ciunkore tum konrusuy ka somo ki ruwe ne kusu, e=eywanke yakka, e=omante yakka, eani e=ramu kuni p e=ki easkay. Tan aeywankep e=asuraste yakka tan sos e=omante yakka en=kore yakun, eci=eyairayke wa.',
+		githubRepository: 'GitHub cinuyeuske',
+		announcement: 'Asir asursos',
+		by: 'kar pe'
+	},
+	ui: {
+		selected: 'a=numke p'
+	},
+	dialog: {
+		editing: 'e=nuye kor an',
+		confirm: 'tane pirka',
+		cancel: 'hontomotuye'
+	},
+	confirm: {
+		deleteSentence: 'Tan itaksay a=isamka yak pirka ruwe an?',
+		new: 'Asir itaksay a=kar yak pirka ruwe an? Ipe opitta a=isamka kusu ne.'
+	},
+	menu: {
+		new: 'Asinno kar',
+		import: 'Cinuyep uk',
+		export: 'Cinuyep omante',
+		svg: 'SVG sanke',
+		png: 'PNG sanke',
+		scramble: 'Color a=sinna'
+	}
+};
+
+export default ain;
